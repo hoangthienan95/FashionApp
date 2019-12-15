7a11111d6f77939389ae8ad0a13f0764cb17ea24
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2019-12-14 19:49:56 -0500

    Routes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import json
-import os
 import random
-from typing import List, Tuple, Sequence, Dict
+from typing import List, Tuple, Dict
 
 from flask import Flask, render_template
 
@@ -30,6 +29,61 @@ if __name__ == '__main__':
 
 with app.app_context():
     similarity.load_primary_indexes(db.session)
+
+
+@app.route('/login')
+def login():
+    pass
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/signup')
+def signup():
+    pass
+
+
+@app.route('/wardrobe')
+def wardrobe():
+    pass
+
+
+@app.route('/outfits')
+def outfits():
+    pass
+
+
+@app.route('/creator')
+def outfit_creator():
+    pass
+
+
+@app.route('/api/add_outfit_item')
+def api_add_outfit_item():
+    pass
+
+
+@app.route('/api/remove_outfit_item')
+def api_remove_outfit_item():
+    pass
+
+
+@app.route('/api/create_outfit')
+def api_create_outfit():
+    pass
+
+
+@app.route('/api/delete_outfit')
+def api_delete_outfit():
+    pass
+
+
+@app.route('/api/recommend')
+def api_recommend():
+    pass
 
 
 @app.route('/debug')
